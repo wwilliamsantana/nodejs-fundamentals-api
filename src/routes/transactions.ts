d9f7fbd8edd2f5290f85a4068b86d2a26bd908cc
William Santana <euwilliamsantana@gmail.com>
2025-05-23 14:54:49 -0300

    feat: Create transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionSchemaBody = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { amount, title, type } = createTransactionSchemaBody.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
