e06451d22af7de7c40e9d20ff98036122cbd6b36
William Santana <euwilliamsantana@gmail.com>
2025-05-26 11:42:00 -0300

    refactor test: Add Categorization for the tests

---
+++ src/test/transactions.test.ts
@@ -0,0 +1,24 @@
+import { afterAll, beforeAll, describe, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 2000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
