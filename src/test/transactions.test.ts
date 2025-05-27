a812ebf638bda191c5690ae9024a0f061cb4fdb8
William Santana <euwilliamsantana@gmail.com>
2025-05-27 09:12:18 -0300

    test: Create Test of list all transactions

--- src/test/transactions.test.ts
+++ src/test/transactions.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -20,5 +20,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  test('list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 2000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listAllTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listAllTransactions.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 2000,
+      }),
+    ])
   })
 })
