83a60be347979be3019065cff0fc3082e20f1cce
William Santana <euwilliamsantana@gmail.com>
2025-06-07 09:56:51 -0300

    fix: check session type

--- challenge/src/middlewares/verify-cookie-routes.ts
+++ challenge/src/middlewares/verify-cookie-routes.ts
@@ -1,10 +1,15 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 
 export async function checkForRouteCookies(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const { sessionId } = request.cookies
+  const checkSessionIdSchema = z.object({
+    sessionId: z.string().uuid(),
+  })
+
+  const { sessionId } = checkSessionIdSchema.parse(request.cookies)
 
   if (!sessionId) {
     return reply.status(404).send({
